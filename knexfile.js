51593bb3cf998cc2098b831771e0aa5ee3a918bc
markporo <markporo@yahoo.com>
2021-12-18 19:10:53 -0500

    getting ready for heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,12 @@
+require('dotenv').config()
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
 const sharedConfig = {
-  client: 'sqlite3',
+  client: 'pg',
   useNullAsDefault: true,
   migrations: {
     directory: './data/migrations',
@@ -7,20 +14,47 @@ const sharedConfig = {
   seeds: {
     directory: './data/seeds',
   },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done)
-    },
-  },
 }
 
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: { filename: './data/database.db3' },
+    connection: process.env.DEV_DATABASE_URL,
   },
   testing: {
     ...sharedConfig,
-    connection: { filename: './data/testing.db3' },
+    connection: process.env.TESTING_DATABASE_URL,
   },
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  }
 }
+
+// const sharedConfig = {
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   migrations: {
+//     directory: './data/migrations',
+//   },
+//   seeds: {
+//     directory: './data/seeds',
+//   },
+//   pool: {
+//     afterCreate: (conn, done) => {
+//       conn.run('PRAGMA foreign_keys = ON', done)
+//     },
+//   },
+// }
+
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: { filename: './data/database.db3' },
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: { filename: './data/testing.db3' },
+//   },
+// }
